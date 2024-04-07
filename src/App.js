76c598d21e47617ebcf58efb9647b06a00f37ebd
Jameel Ahmed <jameel.webdev@gmail.com>
2024-04-07 10:56:01 +0530

    Registration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Loader from "./components/Loader";
+
+const Register = lazy(() => import("./components/Users/Register"));
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </Suspense>
       </Router>
     </>
   );
---
+++ src/apis/users/usersApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const registerUserApi = async (userData) => {
+  const res = await axios.post(
+    "http://localhost:5000/api/v1/user/register",
+    {
+      username: userData?.username,
+      email: userData?.email,
+      password: userData?.password,
+    },
+    {
+      withCredentials: true,
+    }
+  );
+  return res?.data;
+};
